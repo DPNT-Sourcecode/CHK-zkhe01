808cc95f7e8030a43c500e326076210db0c4d8c0
sbx_user1051 <sbx_user1051@169.254.74.101>
2022-07-24 14:20:58 +0000

    Sun Jul 24 14:20:58 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,11 +3,24 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    def invalid_input(skus):
+        if not skus.isalnum():
+            return True
+        for i in range(1, len(skus)):
+            if skus[i].isalpha() and skus[i-1].isalpha():
+                return True
+
+        return False
+
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
     num_item_offer = {'A': 3, 'B': 2}
     multi_price = {'A': 130, 'B': 45}
     i = 0
     total = 0
+
+    if invalid_input(skus):
+        return -1
+
     while i < len(skus):
         units = 0
         while skus[i].isdigit():
@@ -27,7 +40,8 @@ def checkout(skus):
     return total
 
 
-print(checkout("4A3B3C"))
+print(checkout("2"))
+
 
 
 
