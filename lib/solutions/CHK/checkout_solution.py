8df07a83c344a4fab3007f24b8ef578d335c397e
sbx_user1051 <sbx_user1051@169.254.74.101>
2022-07-24 15:35:37 +0000

    Sun Jul 24 15:35:37 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,12 +2,15 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+from sre_constants import CHCODES
+
+
 def checkout(skus):
     item_price = {'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40, 'F': 10}
     order = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0}
     num_item_offer = {'A': {5, 3}, 'B': 2}
     multi_products = {'E': {2: 'B'}, 'F': {3: 'F'}}
-    multi_price = {'A': {5: 200, 3: 130}, 'B': 45, 'E': 'B', 'F': 'F'}
+    multi_price = {'A': {5: 200, 3: 130}, 'B': 45}
     i = 0
     total = 0
 
@@ -19,9 +22,13 @@ def checkout(skus):
 
     for item, offer in multi_products.items():
         for unit, second_item in offer.items():
-            order[second_item] -= max(0,
-                                      order[second_item] - (order[item]//unit))
+            print(item)
+            print(second_item)
+            print(unit)
+            order[second_item] = max(0,
+                                     order[second_item] - (order[item]//unit))
 
+    print(order)
     # order['B'] -= (order['E']//2)
     # if order['B'] < 0:
     #     order['B'] = 0
@@ -47,4 +54,11 @@ def checkout(skus):
                 total += (discounted + non_discounted)
 
     return total
+
+
+print(checkout("AAAAA"))
+print(checkout("AAAAAEEB"))
+print(checkout("AAAAAEEBBB"))
+print(checkout("AAAAAFFF"))
+
 
