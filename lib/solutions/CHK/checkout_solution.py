14be4891845983b1c1114af7168178b6fc3e4628
sbx_user1051 <sbx_user1051@169.254.74.101>
2022-07-24 14:36:44 +0000

    Sun Jul 24 14:36:44 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,47 +3,36 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    def invalid_input(skus):
-        if not skus.isalnum():
-            return True
-        for i in range(1, len(skus)):
-            if skus[i].isalpha() and skus[i-1].isalpha():
-                return True
-
-        return False
-
-    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    item_price = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    order = {'A': 0, 'B': 0, 'C': 0, 'D': 0}
     num_item_offer = {'A': 3, 'B': 2}
     multi_price = {'A': 130, 'B': 45}
     i = 0
     total = 0
 
-    if invalid_input(skus):
-        return -1
-
     while i < len(skus):
-        units = 0
-        while i < len(skus) and skus[i].isdigit():
-            units = (units*10) + int(skus[i])
-            i += 1
-
-        if i >= len(skus):
+        if skus[i] not in item_price:
             return -1
-
-        item = skus[i]
-
-        if item not in prices:
-            return -1
-
-        if item not in multi_price:
-            total += (units*prices[item])
-        else:
-            discounted = (units//num_item_offer[item])*multi_price[item]
-            non_discounted = (units % num_item_offer[item])*prices[item]
-            total += (discounted + non_discounted)
-
+        order[skus[i]] += 1
         i += 1
 
+    for item, unit in order.items():
+        if item not in multi_price:
+            total += (unit*item_price[item])
+        else:
+            discounted = (unit//num_item_offer[item])*multi_price[item]
+            non_discounted = (unit % num_item_offer[item])*item_price[item]
+            total += (discounted + non_discounted)
+
     return total
+
+
+print(checkout(""))
+print(checkout("A"))
+print(checkout("B"))
+print(checkout("AxA"))
+print(checkout("AAA"))
+print(checkout("AAAA"))
+
 
 
