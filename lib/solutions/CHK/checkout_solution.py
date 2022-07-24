571a22eb27f3c6b0e5fa8e61b8c8dcfc249c62e8
sbx_user1051 <sbx_user1051@169.254.74.101>
2022-07-24 14:08:58 +0000

    Sun Jul 24 14:08:58 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,29 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    print(skus)
+    prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
+    num_item_offer = {'A': 3, 'B': 2}
+    multi_price = {'A': 130, 'B': 45}
+    i = 0
+    total = 0
+    while i < len(skus):
+        units = 0
+        while skus[i].isdigit():
+            units = (units*10) + int(skus[i])
+            i += 1
+
+        item = skus[i]
+        if item not in multi_price:
+            total += (units*prices[item])
+        else:
+            discounted = (units//num_item_offer[item])*multi_price[item]
+            non_discounted = (units % num_item_offer[item])*prices[item]
+            total += (discounted + non_discounted)
+
+        i += 1
+
+    return total
+
+
+print(checkout("1A2B3C1D"))
+
